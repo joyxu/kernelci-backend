e48d7a9a0984a86a10aacb9c65a663dfd326f94d
Wei Xu <xuwei5@hisilicon.com>
2015-06-24 20:08:03 +0100

    test
    Signed-off-by: Wei Xu <xuwei5@hisilicon.com>

--- app/models/job.py
+++ app/models/job.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """The model that represents a job document in the mongodb collection."""
+import bson
 import datetime
 import types
 
